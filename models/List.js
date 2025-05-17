a913db2186b7cb90f385642ce4c00223ef27bdbf
Ashutosh Kumar <kashutosh02@gmail.com>
2025-05-17 17:12:50 +0530

    refactor: Remove counts field from List schema

--- models/List.js
+++ models/List.js
@@ -5,12 +5,6 @@ const ListSchema = new mongoose.Schema(
     partName: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String },
-    counts: [
-      {
-        _id: { type: mongoose.Schema.Types.ObjectId, auto: false },
-        count: { type: Number, required: false },
-      },
-    ],
   },
   { timestamps: true }
 );
